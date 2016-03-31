88f5786aef88d41ac926aef54d731d130f763869
rafaelnsantos <rafaelnsantos@users.noreply.github.com>
2016-03-30 21:40:56 -0300

    Update Dimmer.h

--- Dimmer.h
+++ Dimmer.h
@@ -5,12 +5,8 @@
 #ifndef _DIMMER_h
 #define _DIMMER_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
-	#include "Arduino.h"
-#else
-	#include "WProgram.h"
-#endif
-
+#include "Arduino.h"
+	
 class Dimmer
 {
  protected:
